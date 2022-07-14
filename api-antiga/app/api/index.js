e9adf012d122a9920e4b5316dbeda55e972c2a03
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-07-14 16:32:10 -0300

    Removida API antiga e adicionado gitignore na API Node

--- api-antiga/app/api/index.js
+++
@@ -1,5 +0,0 @@
-const userAPI = require("./user");
-const vehicleAPI = require("./vehicle");
-const vehicledataAPI = require("./vehicledata");
-
-module.exports = {  userAPI, vehicleAPI, vehicledataAPI };
--- api-antiga/app/routes/index.js
+++
@@ -1,5 +0,0 @@
-const userRoutes = require("./user");
-const vehicleRoutes = require("./vehicle");
-const vehicledataRoutes = require("./vehicledata");
-
-module.exports = { vehicledataRoutes, userRoutes, vehicleRoutes };
